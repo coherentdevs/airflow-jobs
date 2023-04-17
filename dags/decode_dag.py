413a4e2497f978837b1000ad3009e1b98f700da5
Eugene Tian <eugene12551@gmail.com>
2023-04-17 13:29:14 -0400

    create_snowflake_temp_tables_for_dbt_incremental_builds

--- dags/decode_dag.py
+++ dags/decode_dag.py
@@ -1,8 +1,11 @@
 from datetime import timedelta
 from airflow import DAG
 from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
-from dbt_cloud_operator import DbtCloudOperator
-from datetime import datetime
+from airflow.operators.python_operator import BranchPythonOperator
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime, timedelta
+import logging
+import os
 
 # Define default arguments for the DAG
 default_args = {
@@ -12,35 +15,131 @@ default_args = {
     "email_on_retry": False,
     "retries": 1,
     "retry_delay": timedelta(minutes=5),
+    "start_date": datetime.now() - timedelta(days=1),
 }
 
 # Instantiate the DAG
 dag = DAG(
     "decode_dag",
     default_args=default_args,
-    description="DAG for decoding data using two SQL scripts",
-    schedule_interval=timedelta(minutes=1),
-    start_date=datetime(2023, 4, 11),
+    description="DAG to load parquet files from GCS to Snowflake and run incremental dbt models",
+    schedule_interval=None,
     catchup=False,
 )
 
-# Task 1: Join raw data with method or event fragments
-join_raw_data = SnowflakeOperator(
-    task_id="join_raw_data",
-    sql="path/to/join_raw_data.sql", # TODO
-    snowflake_conn_id="your_snowflake_connection", # TODO
-    autocommit=True,
+
+def extract_gcs_path(**kwargs):
+    gcs_path = kwargs['dag_run'].conf['name']
+    gcs_path_split = gcs_path.split("/")
+    type = gcs_path_split[0]
+    temp_table_name = gcs_path.replace(".parquet", "").replace("/", "_").replace("-", "_")
+    kwargs["ti"].xcom_push("type", type)
+    kwargs["ti"].xcom_push("gcs_path", gcs_path)
+    kwargs["ti"].xcom_push("temp_table_name", temp_table_name)
+
+    logging.info(f"Extracted gcs_path: {gcs_path}")
+
+extract_gcs_path_task = PythonOperator(
+    task_id="extract_gcs_path",
+    python_callable=extract_gcs_path,
+    provide_context=True,
     dag=dag,
 )
 
-# Task 2: Run decoding DBT job
-run_decoding_dbt_job = SnowflakeOperator(
-    task_id="run_decoding_dbt_job",
-    sql="path/to/run_decoding_dbt_job.sql", # TODO
-    snowflake_conn_id="your_snowflake_connection", # TODO
-    autocommit=True,
+def choose_branch(**kwargs):
+    type = kwargs["ti"].xcom_pull(key="type")
+
+    if type == "traces":
+        return "load_traces_to_temp_table"
+    elif type == "blocks":
+        return "load_blocks_to_temp_table"
+    elif type == "transactions":
+        return "load_transactions_to_temp_table"
+    elif type == "logs":
+        return "load_logs_to_temp_table"
+    else:
+        raise ValueError(f"Unexpected table name: {type}")
+
+branch_task = BranchPythonOperator(
+    task_id="branch_task",
+    python_callable=choose_branch,
+    provide_context=True,
+    dag=dag,
+)
+def log_success(context):
+    logging.info("Successfully loaded parquet file into temp table")
+
+
+def log_failure(context):
+    logging.error("Failed to load parquet file into temp table")
+
+
+load_traces_to_temp_table = SnowflakeOperator(
+    task_id="load_traces_to_temp_table",
+    sql="""
+        CREATE OR REPLACE TEMPORARY TABLE temporary_incremental.{{ ti.xcom_pull(key='temp_table_name') }} LIKE ETHEREUM_MANAGED.RAW.TRACES;
+        COPY INTO temporary_incremental.{{ ti.xcom_pull(key='temp_table_name') }}
+            FROM @ETHEREUM_MANAGED.RAW.ETH_RAW_STAGE
+            file_format = parquet_format
+            files = ('{{ ti.xcom_pull(key='gcs_path') }}')
+            MATCH_BY_COLUMN_NAME = CASE_INSENSITIVE;
+    """,
+    snowflake_conn_id="snowflake_temporary_incremental",
+    on_success_callback=log_success,
+    on_failure_callback=log_failure,
     dag=dag,
 )
 
-# Set task dependencies
-join_raw_data >> run_decoding_dbt_job
+load_blocks_to_temp_table = SnowflakeOperator(
+    task_id="load_blocks_to_temp_table",
+    sql="""
+        CREATE OR REPLACE TEMPORARY TABLE temporary_incremental.{{ ti.xcom_pull(key='temp_table_name') }} LIKE ETHEREUM_MANAGED.RAW.BLOCKS;
+        COPY INTO temporary_incremental.{{ ti.xcom_pull(key='temp_table_name') }}
+            FROM @ETHEREUM_MANAGED.RAW.ETH_RAW_STAGE
+            file_format = parquet_format
+            files = ('{{ ti.xcom_pull(key='gcs_path') }}')
+            MATCH_BY_COLUMN_NAME = CASE_INSENSITIVE;
+    """,
+    snowflake_conn_id="snowflake_temporary_incremental",
+    on_success_callback=log_success,
+    on_failure_callback=log_failure,
+    dag=dag,
+)
+
+load_transactions_to_temp_table = SnowflakeOperator(
+    task_id="load_transactions_to_temp_table",
+    sql="""
+        CREATE OR REPLACE TEMPORARY TABLE temporary_incremental.{{ ti.xcom_pull(key='temp_table_name') }} LIKE ETHEREUM_MANAGED.RAW.TRANSACTIONS;
+        COPY INTO temporary_incremental.{{ ti.xcom_pull(key='temp_table_name') }}
+            FROM @ETHEREUM_MANAGED.RAW.ETH_RAW_STAGE
+            file_format = parquet_format
+            files = ('{{ ti.xcom_pull(key='gcs_path') }}')
+            MATCH_BY_COLUMN_NAME = CASE_INSENSITIVE;
+    """,
+    snowflake_conn_id="snowflake_temporary_incremental",
+    on_success_callback=log_success,
+    on_failure_callback=log_failure,
+    dag=dag,
+)
+
+load_logs_to_temp_table = SnowflakeOperator(
+    task_id="load_logs_to_temp_table",
+    sql="""
+        CREATE OR REPLACE TEMPORARY TABLE temporary_incremental.{{ ti.xcom_pull(key='temp_table_name') }} LIKE ETHEREUM_MANAGED.RAW.LOGS;
+        COPY INTO temporary_incremental.{{ ti.xcom_pull(key='temp_table_name') }}
+            FROM @ETHEREUM_MANAGED.RAW.ETH_RAW_STAGE
+            file_format = parquet_format
+            files = ('{{ ti.xcom_pull(key='gcs_path') }}')
+            MATCH_BY_COLUMN_NAME = CASE_INSENSITIVE;
+    """,
+    snowflake_conn_id="snowflake_temporary_incremental",
+    on_success_callback=log_success,
+    on_failure_callback=log_failure,
+    dag=dag,
+)
+
+extract_gcs_path_task >> branch_task
+branch_task >> load_traces_to_temp_table
+branch_task >> load_blocks_to_temp_table
+branch_task >> load_transactions_to_temp_table
+branch_task >> load_logs_to_temp_table
