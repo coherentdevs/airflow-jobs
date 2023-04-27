f9e1c2165665533a90f195b3e9c5e5e4f8b50015
Eugene Tian <eugene12551@gmail.com>
2023-04-27 16:15:25 -0400

    add backfill optimism job

--- dags/common/optimism_queries.py
+++ dags/common/optimism_queries.py
@@ -129,3 +129,13 @@ CREATE or REPLACE PIPE optimism_raw_logs
 COPY INTO LOGS
 FROM @optimism_raw_stage/logs/ file_format = parquet_format match_by_column_name = case_insensitive pattern = '(.+)\.parquet';
 """
+
+OPTIMISM_COPY_FMT = """
+COPY INTO
+    {table_name}
+FROM
+	@optimism_raw_stage/{object_type}/blocks_{start}-{end}/
+	    file_format = parquet_format 
+	    match_by_column_name = case_insensitive 
+	    pattern = '^(.+)\.parquet$'
+"""
