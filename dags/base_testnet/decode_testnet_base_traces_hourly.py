953a60f5b7df231e4fc6d11eaedfd536d115cf58
Eugene Tian <eugene12551@gmail.com>
2023-05-11 11:25:24 -0400

    add BASE incremental jobs

---
+++ dags/base_testnet/decode_testnet_base_traces_hourly.py
@@ -0,0 +1,34 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.operators.bash_operator import BashOperator
+
+# Define default arguments for the DAG
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 0,
+    "retry_delay": timedelta(minutes=5),
+    "start_date": datetime.now() - timedelta(days=1),
+}
+
+# Instantiate the DAG
+dag = DAG(
+    "decode_testnet_base_traces_hourly",
+    default_args=default_args,
+    description="DAG to decode testnet_base traces hourly",
+    schedule_interval='0 * * * *',
+    catchup=False,
+)
+
+
+run_incremental_testnet_base_traces_dbt_model = BashOperator(
+    task_id='run_incremental_testnet_base_traces_dbt_model',
+    bash_command='cd /home/airflow/gcs/dags/evm-models/ && dbt run --select decoded_testnet_base_traces '
+                 '--target testnet-base-production --vars \"{"testnet_base_raw_database": '
+                 '"base_managed", "contracts_database": "contracts"}\" ',
+    dag=dag
+)
+
+run_incremental_testnet_base_traces_dbt_model
