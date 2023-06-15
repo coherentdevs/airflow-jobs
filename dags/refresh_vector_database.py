ad1af35d075013f0da7d32a5329a24741fac2d54
Robin Arenson <robin.arenson@gmail.com>
2023-06-14 20:53:50 -0700

    Pull from env vars and process in batches of 1000

--- dags/refresh_vector_database.py
+++ dags/refresh_vector_database.py
@@ -2,14 +2,16 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.providers.http.operators.http import SimpleHttpOperator
 from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
+from airflow.models import Variable
 from datetime import datetime, timedelta
 from common.pinecone import PineconeClient
 from common import config
 
 SNOWFLAKE_CONN_ID="snowflake_ethereum"
 VECTORS_ENDPOINT="http://api.semantic-beta.coherent.xyz/vectors"
-PINECONE_NAMESPACE="your_pinecone_namespace"
-PINECODE_API_KEY=""
+PINECONE_NAMESPACE="user_vectors"
+PINECODE_API_KEY=Variable.get("pinecode_api_key")
+BATCH_SIZE = 1000
 
 default_args = {
     "owner": "airflow",
@@ -22,9 +24,7 @@ default_args = {
     "snowflake_conn_id": SNOWFLAKE_CONN_ID
 }
 
-def upsert_to_pinecone(**context):
-    pinecone = PineconeClient(PINECODE_API_KEY)
-    vectors = context['task_instance'].xcom_pull(task_ids='get_vectors')
+def upsert_to_pinecone(vectors, pinecone):
     pinecone.upsert_vectors(vectors, PINECONE_NAMESPACE)
 
 with DAG(
@@ -36,24 +36,30 @@ with DAG(
         schedule_interval=None,
         default_args=default_args,
 ) as dag:
+
     fetch_addresses = SnowflakeOperator(
         task_id="fetch_addresses",
         sql="SELECT DISTINCT from_address, to_address FROM ETHEREUM.DECODED.DECODED_TRANSACTIONS",
     )
 
-    get_vectors = SimpleHttpOperator(
-        task_id='get_vectors',
-        method='GET',
-        endpoint=VECTORS_ENDPOINT,
-        data=lambda ti: ti.xcom_pull(task_ids='fetch_addresses'),
-        headers={"Content-Type": "application/json"},
-        response_filter=lambda response: response.json(),
-    )
+    addresses = fetch_addresses.execute(context=None)
+    pinecone = PineconeClient(PINECODE_API_KEY)
+    for i in range(0, len(addresses), BATCH_SIZE):
+        batch_addresses = addresses[i: i + BATCH_SIZE]
 
-    upsert_to_pinecone = PythonOperator(
-        task_id='upsert_to_pinecone',
-        python_callable=upsert_to_pinecone,
-        provide_context=True,
-    )
+        get_vectors = SimpleHttpOperator(
+            task_id=f'get_vectors_{i}',
+            method='GET',
+            endpoint=VECTORS_ENDPOINT,
+            data=batch_addresses,
+            headers={"Content-Type": "application/json"},
+            response_filter=lambda response: response.json(),
+        )
 
-    fetch_addresses >> get_vectors >> upsert_to_pinecone
+        upsert_to_pinecone = PythonOperator(
+            task_id=f'upsert_to_pinecone_{i}',
+            python_callable=upsert_to_pinecone,
+            op_kwargs={'vectors': get_vectors.execute(context=None), 'pinecone': pinecone},
+        )
+
+        fetch_addresses >> get_vectors >> upsert_to_pinecone
